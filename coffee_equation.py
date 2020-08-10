1feb643438e491663d70f6e08cadc9f238b436ae
bradloudis <69406122+bradloudis@users.noreply.github.com>
2020-08-10 18:53:33 -0400

    Update coffee_equation.py

--- coffee_equation.py
+++ coffee_equation.py
@@ -1,11 +1,9 @@
 # function that returns weight of water for brewing
 
-
 def kohi(weight):
     grams = 60.
     liter = 1000.
     ratio = liter / grams
     return ratio * weight
 
-
-print(kohi(10.7))
+print(kohi())
